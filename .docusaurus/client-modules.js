c3f510a6dc4c542e75c2355a1c56d459df5d60ba
chefsale <sandi.fatic@gmail.com>
2022-07-12 19:56:28 +0100

    respect gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/chefsale/workspace/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/chefsale/workspace/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/chefsale/workspace/docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/chefsale/workspace/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/chefsale/workspace/docs/src/css/custom.css'),
-];
