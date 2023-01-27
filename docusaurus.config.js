6598a923edc50bc867b4f7f1b776e483e50be2c8
chefsale <sandi.fatic@gmail.com>
2023-01-27 18:19:02 +0000

    fix: sidebar colors

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,9 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/okaidia');
-
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Calimero Documentation",
