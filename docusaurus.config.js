5e876c67af285eeffef1ac171ba1c5b35ae1f073
Favour Kelvin <favourkelvin17@gmail.com>
2023-04-06 23:03:21 +0100

    Add calimero logo as favicon (#15)
    * Add calimero logo as favicon
    
    * add favicon image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "img/logo_calimero.jpeg",
   organizationName: "calimero-is-near", // Usually your GitHub org/user name.
   projectName: "docs", // Usually your repo name.
 
